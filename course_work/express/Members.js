21e5fde2b6ffa18759d06d71c011d4e0bbb219ed
abitarafan <ali.bitarafan@abo.fi>
2021-05-04 17:10:03 +0300

    Completed express tutorials. Exercise files found under coursework/express

---
+++ course_work/express/Members.js
@@ -0,0 +1,28 @@
+const members = [
+    {
+        id: 1,
+        name: 'Ali Bitarafan',
+        email: 'ali@email.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Amir Bitarafan',
+        email: 'amir@email.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Kati Bitarafan',
+        email: 'kati@email.com',
+        status: 'active'
+    },
+    {
+        id: 4,
+        name: 'Kia Bitarafan',
+        email: 'kia@email.com',
+        status: 'active'
+    }
+]
+
+module.exports = members;
